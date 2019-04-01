e212e37c90e9576ff54c1d02cbf83b7d5259d6a8
Ditho Ardiansyah Pulungan <pulungd@kaust.edu.sa>
2019-04-01 16:25:33 +0300

    reading data and filtering golay done
    reading data and filtering golay done.
    
    golay length depends on the format of the file.
    
    .csv from Instron and generally has larger amount of data, so golay frame len is bigger (1500 in this case).
    For .xlsx, it is DIC data containing smaller number of data in general.

---
+++ readmyexcel.m
@@ -0,0 +1,32 @@
+function [myinitdata,newdata_filtered] = readmyexcel(filename)
+%UNTITLED2 Summary of this function goes here
+%Detailed explanation goes here
+%%%%%%read raw data%%%%%%%%%%
+filename = 'a.csv';
+rawdata = xlsread(filename);
+[rownum, colnum] = size(rawdata);
+
+[~, ~, fExt] = fileparts(filename);
+switch lower(fExt)
+  case '.csv'
+    golaylen = 1501;
+    datastartatrow = 12-4;
+    rownum = rownum-datastartatrow + 1;
+    rawdata = rawdata(datastartatrow:end,:);
+  case '.xlsx'
+    golaylen = 25;
+  otherwise  % Under all circumstances SWITCH gets an OTHERWISE!
+    error('Unexpected file extension: %s', fExt);
+end
+
+
+myinitdata = zeros(rownum,colnum);
+newdata_filtered = zeros(rownum,colnum);
+
+for i=1:colnum
+    myinitdata(:,i) = rawdata(:,i);
+    newdata_filtered(:,i) = sgolayfilt(rawdata(:,i),2,golaylen); 
+end
+
+end
+
