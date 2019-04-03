f70b0e8f3196fdeea9f317846ebbd1aa16c1a131
dithoap <dithoap@gmail.com>
2019-04-03 19:34:08 +0300

    update olahan

--- singkeron.m
+++ singkeron.m
@@ -2,16 +2,18 @@ clc;
 clear;
 close all
 
-filename = 'BY60M-2';
+filename = 'BY60M-4';
 stressformat = '.xlsx'; %STRESS
 strainformat = '.csv'; 
-dic_time_offset = 2.2; %in seconds
+dic_time_offset = 1.5; %in seconds
 
 
-[init1, out1] = readmyexcel(strcat(filename,strainformat)); 
+[init1, out1] = readmyexcel(strcat(filename,strainformat)); %multiply by 100 to be in percent
+init1 = 100*init1;
+out1 = 100*out1;
 
 %first plot - strain
-fps = 2.0; %change here for DIC frame per second 
+fps = 2.2; %change here for DIC frame per second 
 
 
 time_interval = 1/fps;
@@ -35,13 +37,13 @@ title('Spline interpolation of DIC \epsilon_{xx} and \epsilon_{yy}');
 xlabel('\bf{Time (s)}', 'FontSize', 14);
 ylabel('\bf{\epsilon_{xx} and \epsilon_{yy} (%)}', 'FontSize', 14);
 
-subplot(3,2,3)
+subplot(3,2,2)
 plot(dic_time,out1(1:num_dic_data,1),'o',sampling_time,strainxx_interp,':.'); hold on;
 plot(dic_time,out1(1:num_dic_data,2),'o',sampling_time,strainyy_interp,':.');
 title('Spline interpolation of DIC \epsilon_{xx} and \epsilon_{yy}');
 xlabel('\bf{Time (s)}', 'FontSize', 14);
 ylabel('\bf{\epsilon_{xx} and \epsilon_{yy} (%)}', 'FontSize', 14);
-axis([0 8 -0.01 0.01])
+axis([0 8 -0.5 0.5])
 
 %this code below read excel/csv file from Instron machine.
 %the data is read by readmyexcel.m file and filtered with Savitzky-Golay
@@ -58,7 +60,7 @@ sigmayy_interp = interp1(out2(:,1),out2(:,5),sampling_time,'spline');
 %used to interpolate stress data as below.
 
 
-subplot(3,2,2)
+subplot(3,2,3)
 plot(out2(:,1),out2(:,5),'o',sampling_time,sigmayy_interp,':.');
 title('Spline interpolation of Instron \sigma_{yy}');
 xlabel('\bf{Time (s)}', 'FontSize', 14);
@@ -73,11 +75,26 @@ axis([-0.5 1 -0.5 1])
 
 FINData = transpose([sampling_time; strainxx_interp; strainyy_interp; sigmayy_interp]); 
 
+subplot(3,2,5)
+plot(strainyy_interp,sigmayy_interp,':.');
+xlabel('\bf{\epsilon_{yy} (%)}', 'FontSize', 14);
+ylabel('\bf{Axial stress \sigma} (MPa)', 'FontSize', 14);
+axis([-0.5 10 -0.5 30])
+
+
+subplot(3,2,6)
+plot(strainyy_interp,sigmayy_interp,':.');
+xlabel('\bf{\epsilon_{yy} (%)}', 'FontSize', 14);
+ylabel('\bf{Axial stress \sigma} (MPa)', 'FontSize', 14);
+axis([-0.5 1 -0.5 10])
+
 x0=700;
-y0=100;
+y0=50;
 width=800;
-height=600;
+height=700;
 set(gcf,'units','points','position',[x0,y0,width,height])
+
+delete(strcat(filename,'_synced.xlsx')); %make sure delete the file bfore creating file with same name
 
 col_header = {'Time (s)', 'StrainXX (%)', 'StrainYY (%)', 'StressYY (MPa)'}; %Row cell array (for column labels)
 xlswrite(strcat(filename,'_synced.xlsx'),FINData,'Sheet1','A2');     %Write data
